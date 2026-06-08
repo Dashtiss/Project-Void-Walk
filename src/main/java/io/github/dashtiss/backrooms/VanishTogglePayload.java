665eba871881a440cada7428d8b015d82c8608a8
dashtiss <brandontisserand@outlook.com>
2026-06-08 13:17:06 -0500

    Added files that were missing

---
+++ src/main/java/io/github/dashtiss/backrooms/VanishTogglePayload.java
@@ -0,0 +1,19 @@
+package io.github.dashtiss.backrooms;
+
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.util.Identifier;
+
+public record VanishTogglePayload() implements CustomPayload {
+    public static final CustomPayload.Id<VanishTogglePayload> ID =
+            new CustomPayload.Id<>(Identifier.of("backrooms", "vanish_toggle"));
+
+    public static final PacketCodec<RegistryByteBuf, VanishTogglePayload> CODEC =
+            PacketCodec.unit(new VanishTogglePayload());
+
+    @Override
+    public CustomPayload.Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+}
